f2d0f34d937de79e6268f63d4304f6c135948b27
MarlonVictor <cmarlonvictor11@gmail.com>
2020-10-14 11:07:04 -0300

    day 2 - Back-end with Node.js

---
+++ server/src/views/images_view.ts
@@ -0,0 +1,14 @@
+import Image from '../models/Image';
+
+export default {
+    render(image: Image) {
+        return {
+            id: image.id,
+            url: `https://localhost:3333/uploads/${image.path}`
+        }
+    },
+    
+    rendeMany(img: Image[]) {
+        return img.map(img => this.render(img))
+    }
+}
